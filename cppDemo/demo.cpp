e34cf56aa8d843020e90fd385a79ee99c5e24cf4
Zhangjiaming0907 <916678091@qq.com>
2019-09-10 14:39:36 +0800

    add cppDemo

---
+++ cppDemo/demo.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int func(int a, int b) ;
+int main{
+    int a; 
+    int b; 
+
+    a=10; 
+    a=a+b ; 
+
+    func(a,b); 
+}
+
+int func ( int a , int b ) {
+    return a+b; 
+}
