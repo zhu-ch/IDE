02ad71493823de3c4535856c60552931181c3246
Zhangjiaming0907 <42411674+Zhangjiaming0907@users.noreply.github.com>
2019-09-12 10:52:32 +0800

    Update demo.cpp

--- Demo/cppDemo/demo.cpp
+++ Demo/cppDemo/demo.cpp
@@ -1,15 +1,12 @@
-#include<stdio.h>
-int func(int a, int b) ;
+#include <stdio.h>
+int func(int a,int b)
 int main(){
-	int a; 
-	int b; 
-
-	a=10; 
+	int a ; 
+	int b ; 
+	a=10 ; 
 	a=a+b ; 
-
-	func(a,b); 
+	func(a ,b) ; 
 }
-
-int func (int a,int b) {
-	return a+b; 
+int func(int a,int b){
+	return a+b ; 
 }
